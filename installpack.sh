b816a39e3b4e8d66b85ce8b91758f2078d988035
DanOlinski <daniel.guterres@hotmail.com>
2025-10-28 15:26:26 -0500

    t

--- installpack.sh
+++ installpack.sh
@@ -1,9 +1,9 @@
-apt-get update
-sudo apt install apache2
-sudo systemctl start apache2
+# apt-get update
+# sudo apt install apache2
+# sudo systemctl start apache2
 sudo apt install curl
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.bashrc
+# source ~/.bashrc
 sudo apt-get install nodejs
 sudo apt install npm
 nvm install 16 
